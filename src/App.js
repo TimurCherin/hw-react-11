5e76a91ccef8844733336149d2e14c27b713f7a9
TimurCherin <109847059+TimurCherin@users.noreply.github.com>
2024-09-02 22:33:37 +0300

    2-stage

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import StickersList from './components/StickersList';
+
+function App() {
+  return (
+    <StickersList />
+  );
+}
+
+export default App;
---
+++ src/components/StickersList.styled.js
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components'
+export const List = styled.ul`
+   list-style: none
+`
+export const ListImg = styled.img`
+   width: 150px;
+   height: 150px;
+`
+export const ListName = styled.h2`
+    font-size: 15px;
+    height: 15px;
+`
